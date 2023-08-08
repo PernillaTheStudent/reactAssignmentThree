ccbca55480f4bc17689f08c5bd31b38f5b3ac867
PernillaTheStudent <pernilla.widerberg@edu.cmeducations.se>
2023-08-08 14:31:27 +0200

    Edit calculateLetters

--- src/calculateLetters.js
+++ src/calculateLetters.js
@@ -1,10 +1,15 @@
 export const calculateLetters = (phrase) => {
     phrase = phrase.replace(/[^a-zA-Z]/g, " "); // change punctuation to space
-    // console.log("step 1", phrase)
+    console.log("step 1", phrase)
     phrase = phrase.replace(/\s+/g, " ").trim();  // consequtive tabs, spaces etc to one space
-    // console.log("step 2", phrase)
+    console.log("step 2", phrase)
     const words = phrase.split(" ");
-    const numberOfWords = words.length;
+    let numberOfWords;
+    if (words[0] === "") {
+        numberOfWords = 0;
+    } else {
+        numberOfWords = words.length;
+    }
     const numberOfCharacters = words.join("").length;
 
     return { words: numberOfWords, letters: numberOfCharacters };
